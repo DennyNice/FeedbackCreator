b9ed05b83d4a8e80a49534b96065799fd44d7222
DennyNice <dennynice@icloud.com>
2021-05-19 16:47:27 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Main page</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+</head>
+<body class="bg-dark">
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-mb-4 bg-dark text-white border-bottom">
+    <h5 class="my-0 mr-md-auto font-weight-normal">Feedback creator</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class=" py-2 text-white " href="/">Main page</a>
+        <a class=" py-2 text-white " href="/about">About us</a>
+    </nav>
+    <a class="btn btn-warning" href="/review">Feedback</a>
+</div>
+</body>
+</html>

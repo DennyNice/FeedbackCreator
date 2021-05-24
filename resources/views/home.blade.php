a2511bf22bcfc7fc5a4e30091c831fc9886d3ca1
DennyNice <dennynice@icloud.com>
2021-05-24 23:47:26 +0200

    fix button feedback on home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
                 <div class="container">
                     <h1 class="display-3">Main page</h1>
                     <p>Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-                    <p><a class="btn btn-warning btn-lg" type="button">Feedback</a></p>
+                    <p><a class="btn btn-warning btn-lg" href="/review">Feedback</a></p>
                 </div>
             </div>
 
